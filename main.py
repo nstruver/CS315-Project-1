0db45a7b5071125bc9a80e3692a740540bb0b1c3
nathanstruver <nathan.struver@gmail.com>
2025-10-28 08:36:13 -0700

    Update main.py
    Fixed the issue where the injury column was only zeros.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 
 def call_api():
     url = "https://api.sportradar.com/mlb/trial/v8/en/league/injuries.json"
-    API_KEY = 'tTKcLzpDz1zoW2wuXRrgBnRxzmeMSOuUPWaAgVf0'
+    API_KEY = '...'
     params = {'api_key':API_KEY}
     response = requests.get(url, params=params)
     response = response.json()
@@ -17,10 +17,8 @@ def flip_name(name):
     while name[j] != ',':
         last_name += str(name[j])
         j+=1
-        
     for j in range(j +2, len(name)):
         first_name +=str(name[j])
-        
     full_name = f"{first_name} {last_name}"
     return full_name
     
@@ -36,7 +34,6 @@ def find_injuries(response):
         team_players = team["players"]
         
         for player in team_players:
-            
             if player["position"] == "P":
                 pitcher_name = player["full_name"]
                 
@@ -54,14 +51,14 @@ def get_name(pitcher):
 
 def create_total_injuries(df, injury_list):
     injury_column = []
-    
-    for injured in injury_list:
-        injured_players = injured['pitcher_name']
-        injury_types = [inj['injury_type'] for inj in injury_list]
-        players_list = df['player_name'].tolist()
+
+    injured_players = [inj['pitcher_name'] for inj in injury_list]
+    injury_types = [inj['injury_type'] for inj in injury_list]
+    players_list = df['player_name'].tolist()
     
     i = 0
     j = 0
+    
     while i < len(players_list):
         player = players_list[i]
         
@@ -69,21 +66,19 @@ def create_total_injuries(df, injury_list):
             injury_column.append(injury_types[j])
             i += 1
             j += 1
-            
         elif j < len(injured_players) and injured_players[j] not in players_list:
             j += 1
-            
         else:
             injury_column.append('0')
             i += 1
-            
+    
     return injury_column
 
             
          
 
     
-df = pd.read_csv("./savant_data.csv")
+df = pd.read_csv("C:/Users/nstru/Downloads/savant_data (1).csv")
 df = flip_names(df)
 df_sorted = df.sort_values(by = "player_name")
 response = call_api()
@@ -92,10 +87,12 @@ sorted_injuries = sorted(injuries, key =get_name)
 
 injury_column = create_total_injuries(df_sorted, sorted_injuries)
 injury_df = pd.DataFrame(injury_column)
-
+#print(injury_column)
 
 df_sorted['injuries'] = injury_column
+print(len(injury_column))
+print(len(df_sorted))
 
-print(df_sorted.head())
+df_sorted.to_csv('df_sorted.csv')
 
-df_sorted.to_csv('df_sorted')
+
